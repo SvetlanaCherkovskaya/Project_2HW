91d482c1a5ed84b19bbfe56be881fd8fbbffcec3
SvetlanaCherkovskaya <79523721004@yandex.ru>
2022-05-11 19:50:24 +0300

    test

--- src/test/java/lesson6/Votonia2Test.java
+++ src/test/java/lesson6/Votonia2Test.java
@@ -1,7 +1,11 @@
 package lesson6;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import lesson7.CustomLogger;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.JavascriptExecutor;
@@ -10,6 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -29,11 +34,13 @@ public class Votonia2Test {
         WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--start-maximized");
-        driver = new ChromeDriver(chromeOptions);
+        driver = new EventFiringDecorator(new CustomLogger()).decorate(new ChromeDriver(chromeOptions));
         WebDriverManager.chromedriver();
     }
 
     @Test
+    @Feature("Работа с корзиной")
+    @Story("Добавление товара в козину")
     void addGoodToCartTest() throws InterruptedException{
         driver.get(VotoniaSrartURL);
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
@@ -51,8 +58,8 @@ public class Votonia2Test {
                  .checkAddToCart();
     }
 
-    @AfterAll
-    static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         driver.quit();
     }
 }
